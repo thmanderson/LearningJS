f9d6e50253da1cbca15443765300b4766d12061d
Tom Anderson <t.mcgrath.anderson@gmail.com>
2022-08-10 17:29:05 +0100

    Add content to main page

--- home.js
+++ home.js
@@ -1,11 +1,34 @@
 'use strict';
-// Testing a comment
-console.log("Testing console.log()");
 
-let welcome = 'Welcome';
-let price = 49.99,
-    productName = 'Hiking boots';
+const cssButton = document.getElementById('button-css');
+const jsButton = document.getElementById('button-js');
+const cssText = document.getElementById('content-css');
+const jsText = document.getElementById('content-js');
 
-showMessage("change", welcome);
+// Show CSS content, hide JS content
+cssButton.addEventListener('click', function() {
+    if (cssText.classList.contains('d-none')){
+        cssText.classList.remove('d-none');
+    }
+    else {
+        cssText.classList.add('d-none')
+    }
 
-showMessage("currentOffer", 'We are selling ' + productName + ' for ' + price);
+    if (!jsText.classList.contains('d-none')){
+        jsText.classList.add('d-none');
+    }
+});
+
+// Show JS content, hide CSS content
+jsButton.addEventListener('click', function() {
+    if (jsText.classList.contains('d-none')){
+        jsText.classList.remove('d-none');
+    }
+    else {
+        jsText.classList.add('d-none')
+    }
+
+    if (!cssText.classList.contains('d-none')){
+        cssText.classList.add('d-none');
+    }
+});
