edb2f518637460762d28c174b62d49cfc39f306e
Tom Anderson <t.mcgrath.anderson@gmail.com>
2022-08-11 13:08:13 +0100

    Add accordion menu

--- home.js
+++ home.js
@@ -32,3 +32,22 @@ jsButton.addEventListener('click', function() {
         cssText.classList.add('d-none');
     }
 });
+
+const acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } 
+    else {
+      panel.style.display = "block";
+    }
+  });
+};
