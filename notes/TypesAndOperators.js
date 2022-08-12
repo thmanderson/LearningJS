36d22acc6e6e2bac6330b813e8b4e36383905f61
Tom Anderson <t.mcgrath.anderson@gmail.com>
2022-08-12 11:42:58 +0100

    Add variable notes

--- notes/TypesAndOperators.js
+++ notes/TypesAndOperators.js
@@ -1,25 +1,3 @@
-// Notes for Types & Operators from PluralSight course
-
-// Variables:
-// Variable names must start with _, $, or a letter - cannot be a number.
-// Usually _variableName is for private variables, $ is for auto-generated usually (probably don't use).
-// Use 'let' instead of 'var' - not as good for catching errors, e.g. if not initialised.
-
-//let variableName = value;
-let total = 100;
-total = 99;
-total -= 5;
-total++;
-
-let someWords = "Hello",
-    someMoreWords = 'Something else';
-
-let someBoolean = true;
-
-const theAnswer = 42;
-
-let myVariableType = typeof someWords;
-
 // Strings
 let firstName = 'Tom';
     lastName = 'Anderson';
